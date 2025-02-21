2aff33a4100e48561b28062d18e3689067a1bbe0
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2025-02-21 22:52:14 +0530

    first commit

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -3,6 +3,7 @@ import { Telegraf } from "telegraf";
 const token = "8104716926:AAHm0WxoBy9BOrVk-S9xSWetjQgBrt34ftQ";
 const bot = new Telegraf(token);
 
+const app_url = "https://telegram-mini-game-app-98vx.vercel.app/";
 bot.start((ctx) =>
   ctx.reply("Welcome", {
     reply_markup: {
@@ -11,7 +12,7 @@ bot.start((ctx) =>
           {
             text: "web app lol",
             web_app: {
-              url: "https://aa29-2401-4900-1c27-a6eb-400c-973-55c0-651f.ngrok-free.app",
+              url: app_url,
             },
           },
         ],
